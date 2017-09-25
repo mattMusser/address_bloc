6dfbc7a38d7b2c95b893afe31a2e262c1ed72b18
mattMusser <matthew.musser@gmail.com>
2017-09-25 14:24:12 -0500

    Assignment 4 WIP

--- models/address_book.rb
+++ models/address_book.rb
@@ -21,7 +21,7 @@ require_relative 'entry'
   def remove_entry(name, phone_number, email)
     index = 0
     entries.each do |entry|
-      if name > entry.name
+      if name == entry.name && phone_number == entry.phone_number
         break
       end
       index+= 1

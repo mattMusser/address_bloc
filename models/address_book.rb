5c224db8eca5748a8d02652e06d7c0981bd9bc57
mattMusser <matthew.musser@gmail.com>
2017-09-27 12:24:07 -0500

    Checkpoint 5 corrected mistake with address_book.rb and address_bloc.rb

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,10 +1,20 @@
- require_relative 'controllers/menu_controller'
+require_relative 'entry'
 
+  class AddressBook
+    attr_reader :entries
 
- menu = MenuController.new
+    def initialize
+      @entries = []
+    end
 
- system "clear"
-
- puts "Welcome to Address Bloc!"
-
- menu.main_menu
+  def add_entry(name, phone_number, email)
+    index = 0
+    entries.each do |entry|
+      if name < entry.name
+        break
+      end
+      index+= 1
+    end
+    entries.insert(index, Entry.new(name, phone_number, email))
+  end
+end

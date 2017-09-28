19ec555d812eb29af1ae76a2a799bb4a3e97496d
mattMusser <matthew.musser@gmail.com>
2017-09-28 14:57:35 -0500

    Assignment 6 Finished

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -89,4 +89,13 @@ RSpec.describe AddressBook do
     end
   end
 
+  describe "#import_from_csv" do
+    it "imports the correct number of entries" do
+      book.import_from_csv("entries_2.csv")
+      book_size = book.entries.size
+
+      expect(book_size).to eq 3
+    end
+  end
+
 end
